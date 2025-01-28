8d97e876a594f2ba8eb811cae55fee2cb9efd57c
Adripain <adriendtz.pro@gmail.com>
2025-01-28 11:37:25 +0100

    add base

---
+++ tp3-efrei-spring/src/main/java/com/efrei/tp3_efrei_spring/Tp3EfreiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.efrei.tp3_efrei_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3EfreiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3EfreiSpringApplication.class, args);
+	}
+
+}
---
+++ tp3-efrei-spring/src/test/java/com/efrei/tp3_efrei_spring/Tp3EfreiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efrei.tp3_efrei_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3EfreiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
